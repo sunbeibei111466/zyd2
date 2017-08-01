8e6b2088fa9a14670f9443f6693d8d35cb6e0bc5
sunbeibei <930184501@qq.com>
2017-08-01 18:13:21 +0800

    Initial commit

---
+++ zyd/app/src/main/java/com/jlgproject/App.java
@@ -0,0 +1,40 @@
+package com.jlgproject;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.umeng.analytics.MobclickAgent;
+import com.jlgproject.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author 王锋 on 2017/4/24.
+ */
+
+public class App extends Application {
+
+    private static Context mContext;
+    public static List<?> images = new ArrayList<>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+        String[] urls = getResources().getStringArray(R.array.url);
+        List list = Arrays.asList(urls);
+        images = new ArrayList(list);
+
+
+        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+        //使用集成测试环境
+//        MobclickAgent.setDebugMode(true);
+        MobclickAgent. startWithConfigure(new MobclickAgent.UMAnalyticsConfig(this,"5962ffb65312dd802a000796", "TengXu_YYB", MobclickAgent.EScenarioType.E_UM_NORMAL));
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
